63dd222e7b772821837fb9464d06153ea3bb5f24
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-02-05 22:19:55 +0330

    learning not found ffor eatch page

--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function reviewsDetails({
   params,
 }: {
   params: Promise<{ productId: string; reviewsId: string }>;
 }) {
   const { productId, reviewsId } = await params;
+  if (parseInt(reviewsId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       {productId} review {reviewsId}
