4f5bb4a12f6748887355048600005a5bdbf23996
Mohsen Baghaei <mohsen.bhi99@gmail.com>
2025-02-27 12:22:47 +0330

    loading and error states

---
+++ src/app/dataFetching/users-server/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      <div className="animate-spin rounded-full size-32 border-b-2 border-white border-solid"></div>
+    </div>
+  );
+}
--- src/data/dataFetching.ts
+++ src/data/dataFetching.ts
@@ -8,4 +8,8 @@ export const DataFetching: DataFetchingType[] = [
     href: "/dataFetching/users-client",
     name: "users-client/Fetching Data in Client Components",
   },
+  {
+    href: "/dataFetching/users-server",
+    name: "users-server/Loading and Error States",
+  },
 ];
